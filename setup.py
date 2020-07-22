d984db90617d53b7a773ab9c5de9db6df7be9eca
Rehmat Alam <contact@rehmat.works>
2020-07-22 16:23:59 +0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='gkpt',
+	version='1.0.0',
+	description='Get ranking for your website in Google search for a particular keyword.',
+	author="SerpsBot",
+	author_email="sales@serpsbot.com",
+	url="https://github.com/serpsbotapi/gkpt",
+	license="MIT",
+	packages=[
+		'gkpt'
+	],
+	install_requires=[
+		'requests'
+	]
+)
